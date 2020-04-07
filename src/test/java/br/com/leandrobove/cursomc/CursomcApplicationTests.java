1b86d1f6f97ca8bc91bd120402aa62296f5bd6a0
Leandro Bove <leandrobove@hotmail.com>
2020-04-07 13:07:11 -0300

    Criação do projeto

---
+++ src/test/java/br/com/leandrobove/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandrobove.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
