44e9eeafbf6112a2d9da153f4a0d4a135f2fb30d
Leandro Bove <leandrobove@hotmail.com>
2020-04-08 16:32:44 -0300

    Criação de endpoint /clientes/{id}

---
+++ src/main/java/br/com/leandrobove/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package br.com.leandrobove.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.leandrobove.cursomc.entities.Cliente;
+import br.com.leandrobove.cursomc.exceptions.ObjectNotFoundException;
+import br.com.leandrobove.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> cliente = repo.findById(id);
+
+		return cliente.orElseThrow(() -> new ObjectNotFoundException(
+				"Cliente não encontrada -  id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
