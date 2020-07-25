bddef3e84f270b2df13871d0967e6213aa3062bd
Leandro Bove <leandrobove@hotmail.com>
2020-07-25 15:55:02 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/br/com/leandrobove/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/leandrobove/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,17 @@
 package br.com.leandrobove.cursomc.resources;
 
+import java.net.URI;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.leandrobove.cursomc.entities.Categoria;
 import br.com.leandrobove.cursomc.services.CategoriaService;
@@ -13,7 +19,7 @@ import br.com.leandrobove.cursomc.services.CategoriaService;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService service;
 
@@ -24,6 +30,15 @@ public class CategoriaResource {
 
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj, HttpServletRequest req) {
+
+		obj = service.insert(obj);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+
 }
--- src/main/java/br/com/leandrobove/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/leandrobove/cursomc/services/CategoriaService.java
@@ -22,4 +22,10 @@ public class CategoriaService {
 				"Categoria não encontrada -  id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		
+		return repo.save(obj);
+	}
+
 }
