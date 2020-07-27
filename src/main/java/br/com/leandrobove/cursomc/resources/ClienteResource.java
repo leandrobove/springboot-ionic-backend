c6754fc0e2371abdadad99712efeca414c984864
Leandro Bove <leandrobove@hotmail.com>
2020-07-27 15:30:09 -0300

    POST, PUT, DELETE e GET para Cliente

--- src/main/java/br/com/leandrobove/cursomc/resources/ClienteResource.java
+++ src/main/java/br/com/leandrobove/cursomc/resources/ClienteResource.java
@@ -1,19 +1,30 @@
 package br.com.leandrobove.cursomc.resources;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.leandrobove.cursomc.dto.ClienteDTO;
 import br.com.leandrobove.cursomc.entities.Cliente;
 import br.com.leandrobove.cursomc.services.ClienteService;
 
 @RestController
 @RequestMapping(value = "/clientes")
 public class ClienteResource {
-	
+
 	@Autowired
 	private ClienteService service;
 
@@ -24,6 +35,64 @@ public class ClienteResource {
 
 		return ResponseEntity.ok().body(obj);
 	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+
+		List<Cliente> listaCliente = service.findAll();
+
+		List<ClienteDTO> listaClienteDTO = new ArrayList<ClienteDTO>();
+
+		for (Cliente c : listaCliente) {
+			listaClienteDTO.add(new ClienteDTO(c));
+		}
+
+		return ResponseEntity.ok().body(listaClienteDTO);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody ClienteDTO clienteDto, HttpServletRequest req) {
+
+		Cliente cliente = service.fromDTO(clienteDto);
+
+		cliente = service.insert(cliente);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cliente.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable Integer id, @RequestBody ClienteDTO clienteDto) {
+		Cliente c = service.fromDTO(clienteDto);
+
+		c.setId(id);
+
+		service.update(c);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
 	
-	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(name = "page", defaultValue = "0") Integer page,
+			@RequestParam(name = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(name = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(name = "direction", defaultValue = "ASC") String direction) {
+
+		Page<Cliente> clientes = service.findPage(page, linesPerPage, orderBy, direction);
+
+		Page<ClienteDTO> listClienteDTO = clientes.map(obj -> new ClienteDTO(obj));
+
+		return ResponseEntity.ok().body(listClienteDTO);
+	}
+
 }
--- src/main/java/br/com/leandrobove/cursomc/services/ClienteService.java
+++ src/main/java/br/com/leandrobove/cursomc/services/ClienteService.java
@@ -1,17 +1,24 @@
 package br.com.leandrobove.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.com.leandrobove.cursomc.dto.ClienteDTO;
 import br.com.leandrobove.cursomc.entities.Cliente;
+import br.com.leandrobove.cursomc.exceptions.DataIntegrityException;
 import br.com.leandrobove.cursomc.exceptions.ObjectNotFoundException;
 import br.com.leandrobove.cursomc.repositories.ClienteRepository;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository repo;
 
@@ -20,5 +27,55 @@ public class ClienteService {
 
 		return cliente.orElseThrow(() -> new ObjectNotFoundException(
 				"Cliente não encontrado -  id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		Cliente c = new Cliente();
+
+		c.setId(clienteDTO.getId());
+		c.setNome(clienteDTO.getNome());
+		c.setEmail(clienteDTO.getEmail());
+
+		return c;
+	}
+
+	public Cliente insert(Cliente cliente) {
+		cliente.setId(null);
+
+		return repo.save(cliente);
+	}
+
+	public Cliente update(Cliente obj) {
+
+		Cliente clienteNovo = find(obj.getId());
+
+		updateData(clienteNovo, obj);
+
+		return repo.save(clienteNovo);
+	}
+
+	private void updateData(Cliente newCliente, Cliente cliente) {
+		newCliente.setNome(cliente.getNome());
+		newCliente.setEmail(cliente.getEmail());
+	}
+
+	public void delete(Integer id) {
+		find(id);
+
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não foi possível excluir, pois o cliente possui relacionamentos.");
+		}
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		return repo.findAll(pageRequest);
 	}
 }
